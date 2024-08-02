258e8bc28739a7e9456868f272530b991fb1ddb9
rafael36832 <caianarafaelcabral@gmail.com>
2024-08-02 17:44:52 -0300

    Rotas iniciais

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { NavComponent } from './components/nav/nav.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: "",
+    component: NavComponent
+}];
