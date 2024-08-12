71cb977354296c7a5855ebb89533d788a5694168
rafael36832 <caianarafaelcabral@gmail.com>
2024-08-12 17:11:48 -0300

    Rota para login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,17 @@
 import { Routes } from '@angular/router';
-import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { NavComponent } from './components/nav/nav.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 
-export const routes: Routes = [{
-    path: '', component: NavComponent, children: [
-        { path: 'home', component: HomeComponent },
-        { path: 'tecnicos', component: TecnicoListComponent}
-    ]
-}];
+export const routes: Routes = [
+    {
+        path: 'login', component: LoginComponent
+    },
+    {
+        path: '', component: NavComponent, children: [
+            { path: 'home', component: HomeComponent },
+            { path: 'tecnicos', component: TecnicoListComponent }
+        ]
+    }
+];
