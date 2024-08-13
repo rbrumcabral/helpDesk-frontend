4f335edbdf3bd3a2e90c30121a3934458ce089f7
rafael36832 <caianarafaelcabral@gmail.com>
2024-08-13 16:04:17 -0300

    Login Component Part 1

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIcon } from '@angular/material/icon';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatIcon, MatButton],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  passwordVisible: boolean = false;
+
+  togglePasswordVisibility() {
+    this.passwordVisible = !this.passwordVisible;
+  }
 }
--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -32,7 +32,7 @@ export class NavComponent {
   constructor(private router: Router){}
 
   ngOnInit(): void{
-    this.router.navigate(['tecnicos']);
+    this.router.navigate(['login']);
   }
 
   logout(): void {
