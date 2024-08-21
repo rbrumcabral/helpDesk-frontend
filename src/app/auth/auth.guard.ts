49bb72bd93bb54b355af52a7c3a10f9993e63e0c
rafael36832 <caianarafaelcabral@gmail.com>
2024-08-20 21:57:38 -0300

    Auth

---
+++ src/app/auth/auth.guard.ts
@@ -0,0 +1,16 @@
+import { ActivatedRouteSnapshot, CanActivateFn, Router, RouterStateSnapshot } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { inject } from '@angular/core';
+
+export const authGuard: CanActivateFn = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  if (authService.isAuthenticated) {
+    return true;
+  }
+
+  router.navigate(['login']);
+  return false;
+};
+
