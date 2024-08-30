0dd3afb4a5618ef45beb1d30079c83308800d7e8
rafael36832 <caianarafaelcabral@gmail.com>
2024-08-30 19:17:25 -0300

    Componente Technician-delete

--- src/app/components/technician/technician-delete/technician-delete.component.ts
+++ src/app/components/technician/technician-delete/technician-delete.component.ts
@@ -1,12 +1,124 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormField, MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
+import { ToastrService } from 'ngx-toastr';
+import { Technician } from '../../../models/technician';
+import { TechnicianService } from '../../../services/technician.service';
 
 @Component({
   selector: 'app-technician-delete',
   standalone: true,
-  imports: [],
+  imports: [MatCheckboxModule, MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule, RouterLink, MatFormField, ReactiveFormsModule, NgxMaskDirective],
+  providers: [provideNgxMask(), ToastrService],
   templateUrl: './technician-delete.component.html',
-  styleUrl: './technician-delete.component.css'
+  styleUrl: './technician-delete.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TechnicianDeleteComponent {
+  form: FormGroup;
+
+  technician: Technician = {
+    'id': '',
+    'name': '',
+    'cpf': '',
+    'email': '',
+    'password': '',
+    'profiles': [],
+    'creationDate': ''
+  }
+
+  constructor(
+    private service: TechnicianService,
+    private toastr: ToastrService,
+    private fb: FormBuilder,
+    private activeRoute: ActivatedRoute,
+    private router: Router,
+  ) {
+    this.toastrConfig();
+    this.form = this.fb.group({
+      adminType: [{ value: false, disabled: true }],
+      clientType: [{ value: false, disabled: true }],
+      technicianType: [{ value: false, disabled: true }],
+      name: ['', [Validators.minLength(6), Validators.required]],
+      cpf: ['', [Validators.minLength(11), Validators.maxLength(11), Validators.required]],
+      email: ['', [Validators.email, Validators.required]],
+      password: ['', [Validators.minLength(8), Validators.required]],
+      confirmPassword: ['', [Validators.minLength(8), Validators.required]]
+    });
+  }
+
+  ngOnInit() {
+    this.technician.id = this.activeRoute.snapshot.paramMap.get('id');
+    this.findById();
+  }
+
+  findById(): void {
+    this.service.findById(this.technician.id).subscribe({
+      next: (response) => {
+        console.log(response);
+        this.technician = response;
+        this.fillForm();
+      },
+      error: (error) => {
+        this.toastr.error("Falha ao recuperar técnico", "Erro");
+      }
+    })
+  }
+
+  delete(): void {
+    this.service.delete(this.technician).subscribe({
+      next: (response) => {
+        this.toastr.success("Usuário deletado com sucesso!", "Sucesso");
+        this.router.navigate(['technician']);
+      },
+      error: (error) => {
+        this.toastr.error("Falha ao deletar o usuário", "Erro");
+        if (error.error.errors) {
+          error.error.errors.forEach(element => {
+            this.toastr.error(element.message);
+          });
+        } else {
+          console.log(error.error);
+          this.toastr.error(error.error.message);
+        }
+      }
+    })
+
+  }
+
+  fillForm() {
+    this.form.get('name').setValue(this.technician.name);
+    this.form.get('cpf').setValue(this.technician.cpf);
+    this.form.get('email').setValue(this.technician.email);
+    this.form.get('password').setValue("");
+
+    this.form.get('adminType').setValue(false);
+    this.form.get('clientType').setValue(false);
+    this.form.get('technicianType').setValue(false);
+
+    if (this.technician.profiles.includes('ADMIN')) {
+      this.form.get('adminType').setValue(true);
+    }
+
+    if (this.technician.profiles.includes('CLIENT')) {
+      this.form.get('clientType').setValue(true);
+    }
+
+    if (this.technician.profiles.includes('TECHNICIAN')) {
+      this.form.get('technicianType').setValue(true);
+    }
+  }
+
+  toastrConfig() {
+    this.toastr.toastrConfig.timeOut = 4000;
+    this.toastr.toastrConfig.closeButton = true;
+    this.toastr.toastrConfig.progressBar = true;
+  }
 
 }
