5f3007ce1afa7ac589de5f686e6888385b2b91a3
rafael36832 <caianarafaelcabral@gmail.com>
2024-08-30 21:27:55 -0300

    Client Service

---
+++ src/app/services/client.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { API_CONFIG } from '../config/api.config';
+import { Technician } from '../models/technician';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClientService {
+
+  constructor(private http: HttpClient) { }
+
+  findById(id: any): Observable<Technician> {
+    return this.http.get<Technician>(`${API_CONFIG.baseUrl}/technician/${id}`);
+  }
+
+  findAll(): Observable<Technician[]> {
+    return this.http.get<Technician[]>(`${API_CONFIG.baseUrl}/technician`);
+  }
+
+  create(technician: Technician): Observable<Technician> {
+    return this.http.post<Technician>(`${API_CONFIG.baseUrl}/technician`, technician);
+  }
+
+  update(technician: Technician): Observable<Technician> {
+    return this.http.put<Technician>(`${API_CONFIG.baseUrl}/technician/${technician.id}`, technician);
+  }
+
+  delete(id: any): Observable<Technician> {
+    return this.http.delete<Technician>(`${API_CONFIG.baseUrl}/technician/${id}`);
+  }
+}
