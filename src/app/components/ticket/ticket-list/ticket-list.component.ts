3330d77bedca5b92ead826f78b577e76be5453dc
rafael36832 <caianarafaelcabral@gmail.com>
2024-08-31 13:08:05 -0300

    Ticket-list inicial

--- src/app/components/ticket/ticket-list/ticket-list.component.ts
+++ src/app/components/ticket/ticket-list/ticket-list.component.ts
@@ -1,12 +1,73 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { RouterLink } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Ticket } from '../../../models/ticket';
+import { TicketService } from '../../../services/ticket/ticket.service';
 
 @Component({
   selector: 'app-ticket-list',
   standalone: true,
-  imports: [],
   templateUrl: './ticket-list.component.html',
-  styleUrl: './ticket-list.component.css'
+  styleUrl: './ticket-list.component.css',
+  imports: [MatFormFieldModule, MatInputModule, MatTableModule, MatSortModule, MatPaginatorModule, MatButtonModule, RouterLink],
+  providers: [ToastrService]
 })
 export class TicketListComponent {
+  displayedColumns: string[] = ['id', 'title', 'client', 'technician', 'acoes'];
+  dataSource: MatTableDataSource<Ticket> = new MatTableDataSource();
 
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  constructor(
+    private service: TicketService,
+    private toastr: ToastrService
+  ) {
+    this.toastrConfig();
+  }
+
+  ngOnInit(): void {
+    this.findAll();
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  findAll() {
+    this.service.findAll().subscribe({
+      next: (response) => {
+        this.dataSource = new MatTableDataSource(response);
+        this.paginator.pageSize=10;
+        this.dataSource.paginator = this.paginator;
+      },
+      error: (error) => {
+        this.toastr.error("Erro ao buscar");
+      }
+    })
+  }
+
+  toastrConfig() {
+    this.toastr.toastrConfig.timeOut = 4000;
+    this.toastr.toastrConfig.closeButton = true;
+    this.toastr.toastrConfig.progressBar = true;
+  }
 }
+
+
