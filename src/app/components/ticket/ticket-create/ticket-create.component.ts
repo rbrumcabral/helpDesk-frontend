8ecb67e231d1e73211f5b901f4b5d8592f06abe1
rafael36832 <caianarafaelcabral@gmail.com>
2024-09-02 17:32:20 -0300

    Inicialização Ticket-create

---
+++ src/app/components/ticket/ticket-create/ticket-create.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ticket-create',
+  standalone: true,
+  imports: [],
+  templateUrl: './ticket-create.component.html',
+  styleUrl: './ticket-create.component.css'
+})
+export class TicketCreateComponent {
+
+}
