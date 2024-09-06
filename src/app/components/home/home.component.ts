4cb5cf6276eb9a335e4fff325be43fe1b89f2fc8
rafael36832 <caianarafaelcabral@gmail.com>
2024-09-06 14:31:51 -0300

    Internacionalização Home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule, TranslateModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
